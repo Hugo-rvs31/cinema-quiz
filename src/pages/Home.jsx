ee4aa8c9b21184272308c1415126898fceb8627a
Hugo-rvs31 <hug.rvs31@gmail.com>
2026-01-23 18:49:03 +0100

    ajout-1.4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,9 +49,3 @@ const Home = () => {
 };
 
 export default Home;
-
-/*Trouvez le film correspondant à l'image en moins de 10 secondes.{" "}
-          <br /> Plus vous trouvez de films, plus votre score final sera élevé,{" "}
-          chaque film vaut 1 point de 1 à 20 films, puis 2 points de 21 à 40
-          films (le jeu fonctionne par palier de 20 films) et ainsi de suite
-          <br /> Êtes-vous prêt à tester vos connaissances cinéma ? */
